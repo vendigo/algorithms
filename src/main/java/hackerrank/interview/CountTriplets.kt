a22d5797a2be56811121a335e339087b89ab0979
vendigo <ace.vendigo@gmail.com>
2018-10-19 16:50:28 +0300

    Completed CountTriplets

--- src/main/java/hackerrank/interview/CountTriplets.kt
+++ src/main/java/hackerrank/interview/CountTriplets.kt
@@ -1,45 +1,24 @@
 import java.math.BigInteger
 
-//TODO: Fix 3 failing tests
 fun countTriplets(arr: Array<Long>, r: Long): String {
-    val valueToIndices = arr.withIndex().groupBy({ it.value }, { it.index })
+    var sum = BigInteger.ZERO
 
-    if (r != 1L) {
-        var sum = 0L
+    val mp2 = mutableMapOf<Long, BigInteger>()
+    val mp3 = mutableMapOf<Long, BigInteger>()
 
-        valueToIndices.keys.forEach { first ->
-            if (valueToIndices.containsKey(first)) {
-                val second = first * r
-                if (valueToIndices.containsKey(second)) {
-                    val third = second * r
-                    if (valueToIndices.containsKey(third)) {
-                        sum += (valueToIndices[first]!!.size * valueToIndices[second]!!.size * valueToIndices[third]!!.size).toLong()
-                    }
-                }
-            }
+    arr.forEach {
+        if (mp3.containsKey(it)) {
+            sum += mp3[it]!!
         }
-        return sum.toString()
+        if (mp2.containsKey(it)) {
+            mp3[it * r] = mp3.getOrDefault(it * r, BigInteger.ZERO) + mp2[it]!!
+        }
+        mp2[it * r] = mp2.getOrDefault(it * r, BigInteger.ZERO) + BigInteger.ONE
     }
 
-    return valueToIndices.values.map { it.size.toLong() }
-            .map { triplets(it) }
-            .multiply()
+    return sum.toString()
 }
 
-private fun List<BigInteger>.multiply(): String {
-    var result = BigInteger.valueOf(1L)
-    this.forEach {
-        result *= it
-    }
-    return result.toString()
-}
-
-fun triplets(size: Long): BigInteger {
-    if (size < 3) {
-        return BigInteger.ZERO
-    }
-    return BigInteger.valueOf(size) * BigInteger.valueOf(size - 1) * BigInteger.valueOf(size - 2) / BigInteger.valueOf(6)
-}
 
 fun main(args: Array<String>) {
     val nr = readLine()!!.trimEnd().split(" ")
